101e25d66d4a8dabd8ba8fb6096a84d6052168d9
brijeshpatel26667 <62686577+brijeshpatel26667@users.noreply.github.com>
2020-08-03 23:24:51 +0530

    Update ngrok-setup.sh

--- ngrok-setup.sh
+++ ngrok-setup.sh
@@ -5,6 +5,16 @@ pkg install figlet -y
 clear
 figlet cYBER cRYPTO
 
+if [-e ngrok]
+then 
+echo "good to go"
+else
+echo -e "\e[3;96mDOWNLOADING NGROK \e[0m"
+cd
+wget --no-check-certificate https://bin.equinox.io/a/e93TBaoFgZw/ngrok-2.2.8-linux-arm.zip
+unzip ngrok-2.2.8-linux-arm.zip
+ clear
+
 echo -e "    \e[101m\e[1;77m:: This Tool Will Help You To Setup NGROK for Port-Forwading :: \e[0m "
 
 read -p $'\n\e[1;36m[\e[0m\e[1;92m*\e[0m\e[1;36m]\e[0m\e[1;92m Enter your Authtoken: \e[0m' auth
