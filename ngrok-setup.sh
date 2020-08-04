39cd86e88e75689fba9a3d75c8e772fc6ee92b39
brijeshpatel26667 <62686577+brijeshpatel26667@users.noreply.github.com>
2020-08-04 15:42:04 +0530

    Update ngrok-setup.sh

--- ngrok-setup.sh
+++ ngrok-setup.sh
@@ -19,11 +19,13 @@ fi
 echo -e "    \e[101m\e[1;77m:: This Tool Will Help You To Setup NGROK for Port-Forwading :: \e[0m "
 
 read -p $'\n\e[1;36m[\e[0m\e[1;92m*\e[0m\e[1;36m]\e[0m\e[1;92m Enter your Authtoken: \e[0m' auth
+cd && ./ngrok authtoken $auth
 read -p $'\n\e[1;36m[\e[0m\e[1;92m*\e[0m\e[1;36m]\e[0m\e[1;92m Enter port for HTTP: \e[0m' port1
 read -p $'\n\e[1;36m[\e[0m\e[1;92m*\e[0m\e[1;36m]\e[0m\e[1;92m Enter port for TCP: \e[0m' port2
 read -p $'\n\e[1;36m[\e[0m\e[1;92m*\e[0m\e[1;36m]\e[0m\e[1;92m Enter port for TCP2: \e[0m' port3
 
-echo "authtoken: $auth" >> .ngrok2/ngrok.yml
+
+echo "authtoken: $auth" > .ngrok2/ngrok.yml
 echo "  " >> .ngrok2/ngrok.yml
 echo "tunnels:" >> .ngrok2/ngrok.yml
 echo "   http:" >> .ngrok2/ngrok.yml
